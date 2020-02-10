0ef45b6d6de720dc6568e5ac199fda0610b12b99
thayron matos <thayronrt@gmail.com>
2020-02-10 05:42:17 -0300

    Foi criado a função para adicionar um paciente na tabela utilizando a função botaoAdicionar que cria um novo campo na tabela utilizando as funções createElement() e appendChild

--- js/principal.js
+++ js/principal.js
@@ -40,6 +40,39 @@ for (var i = 0; i < pacientes.length; i++) {
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log("oi eu cliquei no botão adicionar ");
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    // A funcão creadElemente como o nome ja diz cria um elemente que nesse caso e um tr
+    var pacienteTd = document.createElement("tr");
+    // cria varias td
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    // A funçao appendChild adiciona um filhos ao pacienteTd
+    pacienteTd.appendChild(nomeTd);
+    pacienteTd.appendChild(pesoTd);
+    pacienteTd.appendChild(alturaTd);
+    pacienteTd.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTd);
+
+
+
+
+
 
 });
